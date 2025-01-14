55612faf75db42ca368a57b90b6a11443eb34383
Kevin Quigley <kevinthomassssquigley@gmail.com>
2025-01-14 16:18:28 +1100

    Updating gemini commands

---
+++ Forgemini.md
@@ -0,0 +1,9 @@
+For gemini
+
+The html code file which is attached has a number of words missing. 
+These words include descriptions of the complexity of the algorithms and 
+descriptions of the algorithms. 
+I want you to return the complete html file with these gaps filled in.
+I also want you to remove iframes which are related to leetcode.com. 
+For example <iframe src="https://leetcode.com/playground/mjMSbADc/shared" frameborder="0" width="100%" height="361" name="mjMSbADc"></iframe>.
+These are broken
--- testing.py
+++ testing.py
@@ -1,9 +1,57 @@
-import heapq
-randomNums = [1,5,3,7,9,12,745,3,5,4]
-smallNums = []
+Input = ['add:laptop:5', 'ship:laptop:2', 'ship:laptop:4', 'add:mouse:3', 'ship:mouse:4']
 
-for num in randomNums:
-    heapq.heappush(smallNums,num)
-    #if len(smallNums)>3:
-    #    heapq.heappop(smallNums)
-    print(smallNums)
+
+storageDict = {}
+
+
+
+def addItems(item,number):
+    if item in storageDict:
+        storageDict[item]+=number
+        print(f"{number} " +item+ f" have been added. Total is now {storageDict[item]}")
+        return True
+    else:
+        storageDict[item]=number
+        print(f"{number} " +item+ f" have been added. Total is now {storageDict[item]}")
+        return True
+
+print(addItems("sausages",3))
+print(addItems("sausages",3))
+
+def shipItems(item,number):
+    if not item in storageDict:
+        print("Cant ship "+item+". It is not in stock")
+        return False
+    else:
+        if storageDict[item]<number:
+            print("Cant ship "+item+". There is not enough in stock")
+            return False
+        else:
+            print(f"Successfully shipped {storageDict[item]} of "+item)
+            storageDict[item]-=number
+            return True
+
+
+
+def takeIndividualInput(string):
+    
+    li = string.split(":")
+    if li[0]=="add":
+        valid = addItems(li[1],int(li[2]))
+    elif li[0]=="ship":
+        valid = shipItems(li[1],int(li[2]))
+    else:
+        print("Incorrect item")
+    return valid
+
+print(takeIndividualInput("add:laptop:5"))
+
+
+def takeInput(inputList):
+    validityList = []
+    for string in inputList:
+        validityList.append(takeIndividualInput(string))
+    print(validityList)
+    return validityList
+
+takeInput(Input)
