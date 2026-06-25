a562da5b9d9508ca3dfc60bccddd1fb28c6e3c44
Kevin Quigley <kevin.q@epx.tech>
2026-06-25 11:15:47 +1000

    Adding solution

--- leetcode-py/neetcode-150/45_jump_game_ii/solution.py
+++ leetcode-py/neetcode-150/45_jump_game_ii/solution.py
@@ -1,6 +1,33 @@
+'''
+Imagining it like a sort of level indexing game
+
+Use a stack to keep track of where you are for a given level,
+use the indices available at that level to see where you can jump to next.
+
+n = nums[i] -> this is the available positions
+
+level = 1 -> this is the number of jumps that have taken place
+
+'''
+
 class Solution:
     # Time: O(?)
     # Space: O(?)
     def jump(self, nums: list[int]) -> int:
-        # TODO: Implement jump
-        return 0
+        # TODO: Implement jump - example nums = [2,3,1,1,4] Output: 2
+        n = len(nums)
+
+        def checkLevel(position,level):
+            r= range(position,position+nums[position]+1)
+            if (n-1 in r):
+                return level
+            else:
+                n_v = [checkLevel(i,level+1) for i in r]
+
+                return min(n_v)
+
+
+
+
+
+        return checkLevel(0,1)
--- leetcode-py/neetcode-150/4_median_of_two_sorted_arrays/solution.py
+++ leetcode-py/neetcode-150/4_median_of_two_sorted_arrays/solution.py
@@ -1,6 +1,59 @@
+
+
+
+'''
+How to visualise the split
+
+
+
+
+
+
+'''
+
+
+from math import inf
 class Solution:
     # Time: O(?)
     # Space: O(?)
     def find_median_sorted_arrays(self, nums1: list[int], nums2: list[int]) -> float:
         # TODO: Implement find_median_sorted_arrays
-        return 0.0
+
+        n1=len(nums1)
+        n2 = len(nums2)
+
+        if n1>n2:
+            return self.find_median_sorted_arrays(n2,n1)
+        
+
+        par = (n1+n2)/2
+        b1 = 0
+        b2 = n1
+
+        while b2!=b1:
+            l1 = b1+b2/2
+            r1 = b1+b2/2 +1
+
+            l2 = par-l1+2
+            r2 = l2+1
+
+            l1v = nums1[l1] if l1>=0 else -inf
+            r1v = nums1[r1] if r1<n1 else inf
+            l2v = nums2[l2] if l2>=0 else -inf
+            r2v = nums2[r2] if r2<n2 else inf
+
+            if r1v <l2v:
+                b1 = l1+1
+            elif r1v>l2v:
+                b2 = r1-1
+            else:
+                if par %2:
+                    return (max(l1v,l2v) +min(r1v,r2v))/2
+                else:
+                    return max(l1v,l2v)
+
+
+
+
+        
+
