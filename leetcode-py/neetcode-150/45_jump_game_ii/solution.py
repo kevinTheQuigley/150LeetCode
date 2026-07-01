13198eb705cb1f89935098c4b1f9f6a0dc5c7197
Kevin Quigley <kevin.q@epx.tech>
2026-07-01 13:37:28 +1000

    Working

--- leetcode-py/neetcode-150/45_jump_game_ii/solution.py
+++ leetcode-py/neetcode-150/45_jump_game_ii/solution.py
@@ -1,4 +1,81 @@
 '''
+Greedy algorithm
+
+at any point check what the furthest point you can get to is
+
+Do I just use a while loop?
+
+create a counter that will check what the minimum jumps to get to a point are?
+
+[] -> return 0
+[1] -> return 0
+[1,1] -> return 1
+
+0:{2}
+1:{1,3}
+2:{1,2}
+0 :[0] -> Start at 0
+1:[1] -> 
+2:[1,2]
+3:
+
+'''
+
+
+class Solution:
+    # Time: O(?)
+    # Space: O(?)
+    def jump(self, nums: list[int]) -> int:
+        # TODO: Implement jump - example nums = [2,3,1,1,4] Output: 2
+        jumps=0
+        furthest=0
+        current_jump_max=0
+        n = len(nums)
+
+        for i in range(n-1):
+            furthest = max(furthest,i+nums[i])
+
+            if i == current_jump_max:
+                jumps+=1
+                current_jump_max = furthest
+            
+                if current_jump_max >=n-1:
+                    break
+        return jumps
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 Imagining it like a sort of level indexing game
 
 Use a stack to keep track of where you are for a given level,
@@ -8,7 +85,6 @@ n = nums[i] -> this is the available positions
 
 level = 1 -> this is the number of jumps that have taken place
 
-'''
 
 class Solution:
     # Time: O(?)
@@ -31,3 +107,5 @@ class Solution:
 
 
         return checkLevel(0,1)
+
+'''
--- leetcode-py/neetcode-150/4_median_of_two_sorted_arrays/solution.py
+++ leetcode-py/neetcode-150/4_median_of_two_sorted_arrays/solution.py
@@ -1,59 +1,39 @@
 
 
 
-'''
-How to visualise the split
-
-
-
-
-
-
-'''
-
 
 from math import inf
 class Solution:
     # Time: O(?)
     # Space: O(?)
     def find_median_sorted_arrays(self, nums1: list[int], nums2: list[int]) -> float:
-        # TODO: Implement find_median_sorted_arrays
-
-        n1=len(nums1)
+        n1 = len(nums1)
         n2 = len(nums2)
 
         if n1>n2:
-            return self.find_median_sorted_arrays(n2,n1)
-        
+            return self.find_median_sorted_arrays(nums2,nums1)
 
-        par = (n1+n2)/2
-        b1 = 0
-        b2 = n1
+        hi = n1
+        lo = 0
+        mid = (n1+n2+1)//2
 
-        while b2!=b1:
-            l1 = b1+b2/2
-            r1 = b1+b2/2 +1
+        while hi >=lo:
+            lp1 = (hi+lo)//2
+            lp2 = mid-lp1
 
-            l2 = par-l1+2
-            r2 = l2+1
+            l1=nums1[lp1-1] if (lp1-1>=0) else -inf
+            r1=nums1[lp1] if (lp1<n1) else inf
+            l2=nums2[lp2-1] if (lp2-1>=0) else -inf
+            r2=nums2[lp2] if (lp2<n2) else inf
 
-            l1v = nums1[l1] if l1>=0 else -inf
-            r1v = nums1[r1] if r1<n1 else inf
-            l2v = nums2[l2] if l2>=0 else -inf
-            r2v = nums2[r2] if r2<n2 else inf
-
-            if r1v <l2v:
-                b1 = l1+1
-            elif r1v>l2v:
-                b2 = r1-1
+            if l1 >r2:
+                hi=lp1-1
+            elif l2>r1:
+                lo = lp1+1
             else:
-                if par %2:
-                    return (max(l1v,l2v) +min(r1v,r2v))/2
+                if (n1+n2)%2:
+                    return max (l1,l2)
                 else:
-                    return max(l1v,l2v)
+                    return (max(l1,l2)+min(r1,r2))/2
 
-
-
-
-        
 
